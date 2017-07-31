4c479413f0bfda322d6e5271b27b429c0f42eb19
Tsarar <tsarar3000@gmail.com>
2017-07-31 14:36:02 +0300

    Update Save.cpp

--- Code/cpp/Save.cpp
+++ Code/cpp/Save.cpp
@@ -5,36 +5,36 @@
 		coinss = new std::ostringstream;	//
 		time = new std::ostringstream;		//
 		date = new std::ostringstream;		//
-		levelText = new sf::Text;			//
-		coinsText = new sf::Text;			//
-		timeText = new sf::Text;			//
-		dateText = new sf::Text;			//
-		isEmpty = new bool;					//
+		levelText = new sf::Text;		//
+		coinsText = new sf::Text;		//
+		timeText = new sf::Text;		//
+		dateText = new sf::Text;		//
+		isEmpty = new bool;			//
 
-		font.loadFromFile("fonts/sansation.ttf");				//font loading
+		font.loadFromFile("fonts/sansation.ttf");	//font loading
 
-		levelText->setString("");									//placing level text
-		levelText->setCharacterSize(14);							//
-		levelText->setFont(font);									//
-		levelText->setColor(sf::Color::White);						//
+		levelText->setString("");					//placing level text
+		levelText->setCharacterSize(14);				//
+		levelText->setFont(font);					//
+		levelText->setColor(sf::Color::White);				//
 		levelText->setPosition(70,float(25+70*(numberOfSave-1)));	//
 
-		coinsText->setString("");									//placing coins text
-		coinsText->setCharacterSize(14);							//
-		coinsText->setFont(font);									//
-		coinsText->setColor(sf::Color::White);						//
+		coinsText->setString("");					//placing coins text
+		coinsText->setCharacterSize(14);				//
+		coinsText->setFont(font);					//
+		coinsText->setColor(sf::Color::White);				//
 		coinsText->setPosition(70,float(45+70*(numberOfSave-1)));	//
 
-		timeText->setString("");									//placing time text
-		timeText->setCharacterSize(14);								//
-		timeText->setFont(font);									//
-		timeText->setColor(sf::Color::White);						//
+		timeText->setString("");					//placing time text
+		timeText->setCharacterSize(14);					//
+		timeText->setFont(font);					//
+		timeText->setColor(sf::Color::White);				//
 		timeText->setPosition(270,float(25+70*(numberOfSave-1)));	//
 
-		dateText->setString("");									//placing date text
-		dateText->setCharacterSize(14);								//
-		dateText->setFont(font);									//
-		dateText->setColor(sf::Color::White);						//
+		dateText->setString("");					//placing date text
+		dateText->setCharacterSize(14);					//
+		dateText->setFont(font);					//
+		dateText->setColor(sf::Color::White);				//
 		dateText->setPosition(270,float(45+70*(numberOfSave-1)));	//
 
 		*isEmpty=true;
@@ -53,29 +53,29 @@
 	};
 
 	void Save::draw(){
-		level->str("");								//setting text
-		coinss->str("");							//
-		time->str("");								//
-		date->str("");								//
+		level->str("");						//setting text
+		coinss->str("");					//
+		time->str("");						//
+		date->str("");						//
 		if (getIsEmpty()) levelText->setString("Empty");	//if file is empty
 		else {
-				*level << "Level: " << numOfLevel;			//setting number of level
+				*level << "Level: " << numOfLevel;		//setting number of level
 				levelText->setString( level->str() );		//
 				*coinss << "Coins: " << coins << '/' << maxCoins;//setting coins
 				coinsText->setString( coinss->str() );		//
-				if (hours<10) *time << '0';					//setting time
-				*time << hours << ':';						//
-				if (mins<10) *time << '0';					//
-				*time << mins;								//
-				timeText->setString( time->str() );			//
-				if (day<10) *date << '0';					//setting date
-				*date << day << '.';						//
-				if (month<10) *date << '0';					//
-				*date << month << '.' << year;				//
+				if (hours<10) *time << '0';			//setting time
+				*time << hours << ':';				//
+				if (mins<10) *time << '0';			//
+				*time << mins;					//
+				timeText->setString( time->str() );		//
+				if (day<10) *date << '0';			//setting date
+				*date << day << '.';				//
+				if (month<10) *date << '0';			//
+				*date << month << '.' << year;			//
 				dateText->setString( date->str() ); 		//
-				window.draw(*coinsText);					//
-				window.draw(*timeText);						//
-				window.draw(*dateText);						//
+				window.draw(*coinsText);			//
+				window.draw(*timeText);				//
+				window.draw(*dateText);				//
 			}
 		window.draw(*levelText);					//drawing everything
 
@@ -83,11 +83,11 @@
 
 	void Save::setAll(int _numOfLevel, int _maxCoins, int _coins, int _mins, int _hours, int _day, int _month, int _year) {
 		numOfLevel=_numOfLevel;	//setting information about save
-		maxCoins=_maxCoins;		//
-		coins=_coins;			//
-		mins=_mins;				//
-		hours=_hours;			//
-		day=_day;				//
-		month=_month;			//
-		year=_year;				//
+		maxCoins=_maxCoins;	//
+		coins=_coins;		//
+		mins=_mins;		//
+		hours=_hours;		//
+		day=_day;		//
+		month=_month;		//
+		year=_year;		//
 	};
