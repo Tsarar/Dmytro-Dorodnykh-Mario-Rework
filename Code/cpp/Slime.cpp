ca5e33113cefd7efa4438df562ee11700aebd675
Tsarar <tsarar3000@gmail.com>
2017-07-31 14:21:22 +0300

    Update Slime.cpp

--- Code/cpp/Slime.cpp
+++ Code/cpp/Slime.cpp
@@ -4,8 +4,8 @@
 			sprite->setTexture(image);	//texture load
 			teleport(xPos,yPos);		//his rect plus starting position
 	
-			*dx=float(0.07);			//speed
-			*currentFrame = 0;			//first frame
+			*dx=float(0.07);		//speed
+			*currentFrame = 0;		//first frame
 		};
 	
 	Slime::~Slime() {};
@@ -14,10 +14,10 @@
 			if (*life) {	//interactions only when he is alive, but sprite drawing is needed even when slime dies
 				rect->left += *dx * time;		//unbounding him
 	
-				collision(0);					//blocks interaction
+				collision(0);				//blocks interaction
 
 				*currentFrame = *currentFrame + time * float(0.005);	//frame change
-				if (*currentFrame > 3) *currentFrame = 0;				//cycling
+				if (*currentFrame > 3) *currentFrame = 0;		//cycling
 			};
 
 			if (*life) { (*dx>0) ? sprite->setTextureRect(sf::IntRect(16*int(*currentFrame),32,16,16)) : sprite->setTextureRect(sf::IntRect(16*int(*currentFrame)+16,32,-16,16)); }	//animation
@@ -28,10 +28,10 @@
 		};
 
 	void Slime::collision(int num) {
-		for (int y = int(rect->top/16); y<(rect->top+rect->height)/16; y++)				//height
+		for (int y = int(rect->top/16); y<(rect->top+rect->height)/16; y++)			//height
 			for (int x = int(rect->left/16); x<(rect->left+rect->width)/16; x++)		//width
 				 if ((map.getTileMap(y,x)=='k') || (map.getTileMap(y,x)=='i') || (map.getTileMap(y,x)=='t'))	//solid block
 					{ if (*dx>0)	{ rect->left = x*16 - rect->width; *dx=*dx*(-1); }			//changing direction to opposite
-						else if (*dx<0)	{ rect->left = float(x*16) + 16; *dx=float(*dx*(-1)); }	//										
+						else if (*dx<0)	{ rect->left = float(x*16) + 16; *dx=float(*dx*(-1)); }		//										
 					 }
 		};
