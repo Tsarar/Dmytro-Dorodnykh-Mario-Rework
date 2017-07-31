c4371f2447d47eebf282c42b7281ca67adab8567
Tsarar <tsarar3000@gmail.com>
2017-07-31 14:12:32 +0300

    Update Entity.cpp

--- Code/cpp/Entity.cpp
+++ Code/cpp/Entity.cpp
@@ -3,9 +3,9 @@
 	Entity::Entity(LevelMap &_map) : map(_map){
 		dx = new float;				//heap usage
 		dy = new float;				//
-		rect = new sf::FloatRect;	//
-		sprite = new sf::Sprite;	//
-		currentFrame = new float;	//
+		rect = new sf::FloatRect;		//
+		sprite = new sf::Sprite;		//
+		currentFrame = new float;		//
 		life = new bool;			//
 
 		*life = true;				//default alive
@@ -13,13 +13,13 @@
 	
 	Entity::~Entity() {
 		delete life;		//freeing memory
-		delete currentFrame;//
+		delete currentFrame;	//
 		delete sprite;		//
 		delete rect;		//
-		delete dy;			//
-		delete dx;			//
+		delete dy;		//
+		delete dx;		//
 	}
 	
-	inline void Entity::teleport(int xPos, int yPos)  {						//placing our entity
+	inline void Entity::teleport(int xPos, int yPos)  {					//placing our entity
 				*rect = sf::FloatRect(float(xPos*16),float(yPos*16),16,16);	//at that position
 			}
