852b23fa78009692996427a4cceb3e87ef565364
Tsarar <tsarar3000@gmail.com>
2017-07-31 14:12:00 +0300

    Update DeathMenu.cpp

--- Code/cpp/DeathMenu.cpp
+++ Code/cpp/DeathMenu.cpp
@@ -2,14 +2,14 @@
 
 	DeathMenu::DeathMenu() {
 		yesButton = new sf::Texture;				//dynamic memory usage
-		noButton = new sf::Texture;					//
+		noButton = new sf::Texture;				//
 		textTexture = new sf::Texture;				//
-		yes = new sf::Sprite;						//
-		no = new sf::Sprite;						//
-		text = new sf::Sprite;						//
+		yes = new sf::Sprite;					//
+		no = new sf::Sprite;					//
+		text = new sf::Sprite;					//
 		sizeVector = new sf::Vector2u;				//
-		stand = new int;							//
-		menuNum = new int;							//
+		stand = new int;					//
+		menuNum = new int;					//
 		cooldownClock = new sf::Clock;				//
 
 		yesButton->loadFromFile("images/YesButton.png");	//buttons
@@ -17,27 +17,27 @@
 		textTexture->loadFromFile("images/GameOver.png");	//
 
 		yes->setTexture(*yesButton);		//placing sprites
-		no->setTexture(*noButton);			//
+		no->setTexture(*noButton);		//
 		text->setTexture(*textTexture);		//
-		yes->setPosition(60,175);			//
-		no->setPosition(285,175);			//
-		text->setPosition(0,0);				//
+		yes->setPosition(60,175);		//
+		no->setPosition(285,175);		//
+		text->setPosition(0,0);			//
 
-		*stand=0;							//menu
-		*menuNum=0;							//
+		*stand=0;				//menu
+		*menuNum=0;				//
 	};
 	
 	DeathMenu::~DeathMenu() {
 		delete yesButton;		//freeing memory
 		delete noButton; 		//
 		delete textTexture;		//
-		delete yes;				//
-		delete no;				//
+		delete yes;			//
+		delete no;			//
 		delete text; 			//
 		delete sizeVector; 		//
 		delete stand;			//
 		delete menuNum;			//
-		delete cooldownClock;	//
+		delete cooldownClock;		//
 	};
 
 	bool DeathMenu::run(sf::RenderWindow &window) {
@@ -49,7 +49,7 @@
 						};
 
 					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) if (*menuNum<=1) return 1; else return 0;	//event on pressing enter
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) return 0;									//and escape
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) return 0;					//and escape
 
 					window.clear(sf::Color(100,100,100));	//clearing current frame
 
@@ -57,43 +57,43 @@
 					(*menuNum==1) ? yes->setColor(sf::Color::Green) : yes->setColor(sf::Color::White);	//clearing color																
 					(*menuNum==2) ? no->setColor(sf::Color::Red) : no->setColor(sf::Color::White);		//or coloring
 
-					*menuNum=*stand;					//for arrows interaction
+					*menuNum=*stand;			//for arrows interaction
 
 					*sizeVector = window.getSize();		//if somebody will decide to scretch
 
 					if (sf::IntRect(int(sizeVector->x/6.6667),int(sizeVector->y/1.4285),int(sizeVector->x/4.5),sizeVector->y/7).contains(sf::Mouse::getPosition(window))) { //default:60,175,90,36
-							*stand=0;													//reset buttons
-							*menuNum=1;													//button 'yes'
+							*stand=0;																//reset buttons
+							*menuNum=1;																//button 'yes'
 							if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) return 1;	//restart
 						}
-					if (sf::IntRect(int(sizeVector->x/1.4035),int(sizeVector->y/1.4285),sizeVector->x/7,sizeVector->y/7).contains(sf::Mouse::getPosition(window))) { //default:285,175,57,36
-							*stand=0;													//reset buttons
-							*menuNum=2;													//button 'no'
+					if (sf::IntRect(int(sizeVector->x/1.4035),int(sizeVector->y/1.4285),sizeVector->x/7,sizeVector->y/7).contains(sf::Mouse::getPosition(window))) {//default:285,175,57,36
+							*stand=0;															//reset buttons
+							*menuNum=2;															//button 'no'
 							if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) return 0; 	//exit
 						}
 
 					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {		//move arrows
 						if (cooldownClock->getElapsedTime().asSeconds()>0.15)	//delay
-								{												//
-									*stand=*stand+1;							//
-									if (*stand>2) *stand=1;						//
-									cooldownClock->restart();					//
-								}												//
-						}														//
+								{					//
+									*stand=*stand+1;		//
+									if (*stand>2) *stand=1;		//
+									cooldownClock->restart();	//
+								}				
+						}														
 
 					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {		//move arrows
 						if (cooldownClock->getElapsedTime().asSeconds()>0.15)	//delay
-								{												//
-									*stand=*stand-1;							//
-									if (*stand<1) *stand=2;						//
-									cooldownClock->restart();					//
-								}												//
-						}														//
+								{					//
+									*stand=*stand-1;		//
+									if (*stand<1) *stand=2;		//
+									cooldownClock->restart();	//
+								}												
+						}														
 
-					window.draw(*yes);			//display
+					window.draw(*yes);			//display everything
 					window.draw(*no);			//
 					window.draw(*text);			//
-												//
+										//
 					window.display();			//
 
 			};
