194379280ce8737e2f7c40ebc1d22345f5843466
Tsarar <tsarar3000@gmail.com>
2017-08-20 13:08:14 +0300

    Update SaveWindow.cpp

--- Code/cpp/SaveWindow.cpp
+++ Code/cpp/SaveWindow.cpp
@@ -8,10 +8,10 @@
 												
 		saveTexture.loadFromFile("images/SaveWindow.png");	//loading textures and setting sprites
 		escTexture.loadFromFile("images/Esctoclose.png");	//
-		save1.setTexture(saveTexture);						//
-		save2.setTexture(saveTexture);						//
-		save3.setTexture(saveTexture);						//
-		esc.setTexture(escTexture);							//
+		save1.setTexture(saveTexture);				//
+		save2.setTexture(saveTexture);				//
+		save3.setTexture(saveTexture);				//
+		esc.setTexture(escTexture);				//
 
 		try {
 			initializeSave(1, *saveFile1);		//load save 1
@@ -58,7 +58,7 @@
 
 			sizeVector = window.getSize();																																			//if somebody	
 			if (sf::IntRect(sizeVector.x/10,int(sizeVector.y/17.857),int(sizeVector.x/1.25),int(sizeVector.y/4.4)).contains(sf::Mouse::getPosition(window))) {stand=0; menuNum=1;}	//will decide	//default:40,20,320,57
-			if (sf::IntRect(sizeVector.x/10,int(sizeVector.y/2.9761),int(sizeVector.x/1.25),int(sizeVector.y/4.4)).contains(sf::Mouse::getPosition(window))) {stand=0; menuNum=2;}	//to strech		//default:40,90,320,57
+			if (sf::IntRect(sizeVector.x/10,int(sizeVector.y/2.9761),int(sizeVector.x/1.25),int(sizeVector.y/4.4)).contains(sf::Mouse::getPosition(window))) {stand=0; menuNum=2;}	//to strech	//default:40,90,320,57
 			if (sf::IntRect(sizeVector.x/10,int(sizeVector.y/1.6233),int(sizeVector.x/1.25),int(sizeVector.y/4.4)).contains(sf::Mouse::getPosition(window))) {stand=0; menuNum=3;}	//the window	//default:40,160,320,57
 
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left) ||
@@ -76,37 +76,37 @@
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {		//arrows
 				if (cooldownClock.getElapsedTime().asSeconds() > 0.15){	//delay
-					++stand;								//active button goes down
-					if (stand>3) stand=1;					//cycling
-					cooldownClock.restart();				//
+					++stand;					//active button goes down
+					if (stand>3) stand=1;				//cycling
+					cooldownClock.restart();			//
 				};
 			}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {			//arrows
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {		//arrows
 				if (cooldownClock.getElapsedTime().asSeconds() > 0.15){	//delay
-					--stand;							//active button goes up
+					--stand;				//active button goes up
 					if (stand<1) stand=3;			//cycling
 					cooldownClock.restart();		//
 				};
 			}
 
 			window.clear(sf::Color(100,100,100));	//clearing current frame
-			window.draw(save1);						//draw our save1 background
-			window.draw(save2);						//         save2
-			window.draw(save3);						//         save3
-			window.draw(esc);						//draw our esc message
-			saveFile1->draw();						//draw our save 1 info
-			saveFile2->draw();						//		   save 2
-			saveFile3->draw();						//		   save 3
-			window.display();						//
+			window.draw(save1);			//draw our save1 background
+			window.draw(save2);			//         save2
+			window.draw(save3);			//         save3
+			window.draw(esc);			//draw our esc message
+			saveFile1->draw();			//draw our save 1 info
+			saveFile2->draw();			//	   save 2
+			saveFile3->draw();			//	   save 3
+			window.display();			//
 			};
 		return 0;
 	};
 
 	void SaveWindow::initializeSave(int numOfSave, Save &save){
-		std::ostringstream currentSave;								//get our save from file
-		currentSave << "saves/Save" << numOfSave << ".txt";			//
-		std::ifstream fileWithLevel(currentSave.str());				//
+		std::ostringstream currentSave;					//get our save from file
+		currentSave << "saves/Save" << numOfSave << ".txt";		//
+		std::ifstream fileWithLevel(currentSave.str());			//
 
 		try {
 			if (!(fileWithLevel.good())) {	//if problems with file
@@ -114,14 +114,14 @@
 				}
 			else {
 				int numOfLvl, lives, coins, maxCoins, bufW;	//buffer variables to read info from file
-				bool isEnemiesAlive;						//
-				float oX, oY;								//
-				int month, day, year, hour, min;			//
-				if (fileWithLevel.peek() == -1) numOfLvl=0; //if file is empty
+				bool isEnemiesAlive;				//
+				float oX, oY;					//
+				int month, day, year, hour, min;		//
+				if (fileWithLevel.peek() == -1) numOfLvl=0; 	//if file is empty
 				else fileWithLevel >> numOfLvl >> lives >> coins >> maxCoins >> isEnemiesAlive   //reading
-								   >> oX >> oY >> bufW >> month >> day >> year >> hour >> min;	 //
+						   >> oX >> oY >> bufW >> month >> day >> year >> hour >> min;	 //
 				
-				if (numOfLvl==0) { save.setAll(0,0,0,0,0,0,0,0); save.setIsEmpty(true); }								//if file is empty
+				if (numOfLvl==0) { save.setAll(0,0,0,0,0,0,0,0); save.setIsEmpty(true); }				//if file is empty
 				else { save.setAll(numOfLvl, maxCoins, coins, min, hour, day, month, year); save.setIsEmpty(false); }	//else giving our vars as the parameters to save
 
 				fileWithLevel.close();
@@ -133,20 +133,20 @@
 	};
 
 	int SaveWindow::hashFromFile(int numOfSave) const {
-			std::ostringstream currentSave;							//get our save from file
-			currentSave << "saves/Save" << numOfSave << ".txt";		//
-			std::ifstream fileWithLevel(currentSave.str());			//
+			std::ostringstream currentSave;				//get our save from file
+			currentSave << "saves/Save" << numOfSave << ".txt";	//
+			std::ifstream fileWithLevel(currentSave.str());		//
 
-			int numOfLevel;	//let's read num of level
-			if (!(fileWithLevel.good())) {	//if problems with file
-						throw new Error3;		//we throw error
-						numOfLevel=1;
+			int numOfLevel;					//let's read num of level
+			if (!(fileWithLevel.good())) {			//if problems with file
+						throw new Error3;	//we throw error
+						numOfLevel=1;		//
 					}
 			else {
 				fileWithLevel >> numOfLevel;	//getting number of level
-				fileWithLevel.close();			//
+				fileWithLevel.close();		//
 			}
 
 			return numOfLevel*10+numOfSave;	//returning code
-											//first num is level, second is a save
+							//first num is level, second is a save
 		};
