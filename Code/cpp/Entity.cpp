847ec01fdc625bc1f3298dc3c4354640d94055d6
Tsarar <tsarar3000@gmail.com>
2017-08-20 13:10:22 +0300

    Update Entity.cpp

--- Code/cpp/Entity.cpp
+++ Code/cpp/Entity.cpp
@@ -1,11 +1,11 @@
 #include "../headers/Entity.h"
 
 	Entity::Entity(LevelMap &_map) : map(_map){
-		life = true;				//default alive
+		life = true;	//default alive
 	};
 	
 	Entity::~Entity() {}
 	
-	inline void Entity::teleport(int xPos, int yPos)  {						//placing our entity
+	inline void Entity::teleport(int xPos, int yPos)  {					//placing our entity
 				rect = sf::FloatRect(float(xPos*16),float(yPos*16),16,16);	//at that position
 			}
